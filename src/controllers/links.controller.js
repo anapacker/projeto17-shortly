feabb9b5110f1ae5c0c9a3039ad6d93a92dc10d4
anapacker <anaamaria.packer@gmail.com>
2023-08-06 16:55:50 -0300

    POST/urls/shorten: rota implementada

--- src/controllers/links.controller.js
+++ src/controllers/links.controller.js
@@ -1,10 +1,26 @@
+import { nanoid } from "nanoid";
 import { db } from "../database.js";
 
 export async function shorten(req, res) {
+    const { token } = req.headers
+    const { url } = req.body
+    const shortUrl = nanoid(10)
 
+    if (!token) return res.sendStatus(401)
     try {
+        const findUrl = await db.query(`
+            SELECT * FROM "shortlyUrl" WHERE userId=$1 AND linkId=$2
+        `, [id, url])
+        if (findUrl.rowCount) {
+            res.send({ id: findUrl.rows[0].id, shortUrl: findUrl.rows[0].shortLink })
 
-        res.send("shorten")
+        }
+
+        await db.query(`
+            INSERT INTO "shortlyUrl" ("userId", "linkId", "shortLink")
+            VALUES ($1,$2,$3)
+        `, [id, linkId, shortUrl])
+        return res.status(201).send({ shortUrl: shortLink })
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/links.route.js
+++ src/routes/links.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express"
-import { openShortUrl } from "../controllers/links.controller.js"
+import { openShortUrl, shorten } from "../controllers/links.controller.js"
+import { validateAuth } from "../middlewares/validate.auth.js"
 
 const linkRoute = Router()
-linkRoute.post("/urls/shorten")
+linkRoute.post("/urls/shorten", validateAuth, shorten)
 linkRoute.get("/urls/open/:shortUrl", openShortUrl)
 export default linkRoute
